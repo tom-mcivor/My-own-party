97cb28c13e837cb42a6a31a74e66c068ece8a89f
tommy-tommy-tom <105212576+tom-mcivor@users.noreply.github.com>
2022-11-11 11:22:47 +1300

    add img

--- client/components/Footer.jsx
+++ client/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Footer() {
   return (
-    <footer><p>Made by Mugi, Manu, Tom and James<</footer>
+    <footer><p>Made by Mugi, Manu, Tom and James</p></footer>
 
   )
 }
