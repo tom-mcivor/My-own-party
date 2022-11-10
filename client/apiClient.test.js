1e6881ded1be03a17fed72d62ce476da953934e7
Mugiho <65477315+mugihos@users.noreply.github.com>
2022-11-10 21:23:49 +1300

    add test file with basic mock api test

---
+++ client/apiClient.test.js
@@ -0,0 +1,16 @@
+import nock from 'nock'
+import { getExcuses, getActivities } from './apiClient'
+
+//test for getExcuses API call
+describe('getExcuses', () => {
+  it.todo('get 3 excuses data from api')
+  //add mock call here
+  //test return response
+})
+
+//test for getActivities API call
+describe('getActivites', () => {
+  it.todo('get 1 activity data from api')
+  // add mock call here
+  // test return response
+})
