04765aa61c4765f559b9f366482d945f75548d43
tommy-tommy-tom <105212576+tom-mcivor@users.noreply.github.com>
2022-11-10 17:49:27 +1300

    create getactivities funtion

--- client/components/Activities.jsx
+++ client/components/Activities.jsx
@@ -1,1 +1,39 @@
+import React, { useState, useEffect } from 'react'
+
+import { getActivities } from '../apiClient'
+
+
+function Activities() {
+
+
+const [activities, setActivities] = useState(null)
+
+useEffect(() => {
+
+  getActivities()
+    .then((res) => {
+      // const { latitude, longitude: long } = res
+      // console.log(long, latitude)
+      // setFoxImages(res.fox)
+      setActivities(res)
+                 
+      console.log(res)
+      
+    })
+    .catch((err) => {
+      console.error(err.message)
+    })
+  }, [])
+
+  return(
+  
+  <>
+  <h2>{activities}</h2>
+  
+  </>
+  
+  )
+  
+
+
 export default Activities
